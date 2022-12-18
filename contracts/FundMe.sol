c44c2bb068f153e3d565863e94c5bc2c5fb8628e
SntSwetlana <snt.swetlana@gmail.com>
2022-12-18 08:45:45 +0300

    before testing functionality

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -12,13 +12,20 @@ contract FundMe {
 
     mapping(address => uint256) public addressToAmountFunded;
 
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
     function fund() public payable {
         require(msg.value.getConversionRate() >= minimumUsd, "Didn't send enough!"); //1e18 == 1 * (10**18) = 1000000000000000000
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] = msg.value;
     }
 
-    function withdraw() public {
+    function withdraw() public onluOwner {
+
         for( uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++ ){
             address funder = funders[funderIndex];
             addressToAmountFunded[funder] = 0;
@@ -26,9 +33,19 @@ contract FundMe {
         //reset array
         funders = new address[](0); 
 
-        payable (msg.sender).transfer(address(this).balance);
-    }
+        //transfer
+        // payable (msg.sender).transfer(address(this).balance);
 
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        // require(sendSuccess, "Send failed!");
+
+        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call failed");
+    }
+    modifier onluOwner {
+        require(msg.sender == owner, "Sender is not owner!");
+        _;
+    }
 }
 
 
